5ef3bbf37ef72c31816ef397f57e13cf9f94e92a
raison2808 <00001087698583sp@al.educacao.sp.gov.br>
2024-05-13 08:20:54 -0300

    alterando o nome do index

--- sketch.js
+++ sketch.js
@@ -1,13 +1,14 @@
 function setup() {
-    createCanvas (600, 600); 
+    createCanvas(600, 600);
     background("black");
+}
+  
+function draw() {
+    stroke("blue");
+    fill("red");
+  
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35); 
     }
-    
-    function draw() { 
-        stroke("blue"); 
-        fill("red");
-    
-        if(mouseIsPressed) {
-            rect (mousex, mouseY, 20, 35);
-    }
+  
 }
